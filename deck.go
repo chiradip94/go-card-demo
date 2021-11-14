1bc66dd64ee90c86ecbaddd7505b240eb6a53aec
Chiradip Maji <chiradip.svts@gmail.com>
2021-11-13 20:48:07 -0800

    Adds deck creation and printing

---
+++ deck.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuites := []string{"Spade", "Diamond", "Heart", "Club"}
+	cardValues := []string{"Ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "Jack", "King", "queen"}
+	for _, suit := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i+1, card)
+	}
+
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+var value1 string
+
+func main() {
+	card := newDeck()
+	card.print()
+}
