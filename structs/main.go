10df3ed72f6bec63ef81da5716d208405ac3bd8a
Chiradip Maji <chiradip.svts@gmail.com>
2021-12-08 10:41:03 -0800

    Nested struct

--- structs/main.go
+++ structs/main.go
@@ -2,15 +2,25 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	pincode int
+}
+
 type person struct {
 	firstname string
 	lastname  string
+	contact   contactInfo
 }
 
 func main() {
-	var alex person
-	alex.firstname = "Alex"
-	alex.lastname = "Pope"
-
-	fmt.Printf("%+v", alex)
+	jim := person{
+		firstname: "Jim",
+		lastname:  "Pope",
+		contact: contactInfo{
+			email:   "abc@cde.com",
+			pincode: 1234567,
+		},
+	}
+	fmt.Printf("%+v", jim)
 }
