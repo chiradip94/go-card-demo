9b4f441c2c20c3a9ec29aa3ecf075b014aca34a1
Chiradip Maji <chiradip.svts@gmail.com>
2021-12-16 20:06:20 -0800

    different ways to declare map

--- maps/main.go
+++ maps/main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	colour := map[string]string{
+	/*colour := map[string]string{
 		"red":  "colour1",
 		"blue": "colour2",
-	}
+	}*/
+
+	//var colour map[string]string
+
+	colour := make(map[string]string)
 	fmt.Println(colour)
 }
