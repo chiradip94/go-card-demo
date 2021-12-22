9aaac90f9248f7cc5847411f0174f7a9a226726a
Chiradip Maji <chiradip.svts@gmail.com>
2021-12-22 09:50:06 -0800

    Read body from Get request using Reader Interface

---
+++ Interfaces/HTTP/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://google.com")
+
+	if err != nil {
+		os.Exit(1)
+	}
+	//fmt.Println(resp)
+
+	bs := make([]byte, 99999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
+}
